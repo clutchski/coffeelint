502b91885c51ded2448cb1a1b3bc2a0498fea7a4
clutchski <clutchski@gmail.com>
2011-12-18 23:16:58 -0500

    Show compile errors.

--- js/trycoffeelint.js
+++ js/trycoffeelint.js
@@ -14,30 +14,49 @@
         return table;
     };
 
+
+    var update = function (title, status_, content) {
+        $('.report .diatribe_body').empty().append(content || "").show();
+        $('.report .diatribe_title').text(title);
+        $('.report_row').removeClass('success failure').addClass(status_).slideDown();
+
+    };
+
     var displayReport = function (errors) {
-        var success = errors.length == 0;
 
         var title = 'Your code is lint free!';
+        var status_ = 'success';
         var body = '';
-        var rowClass = 'success';
 
-        var body = $('.report .diatribe_body').empty();
-        body.hide();
-        if (!success) {
+        if (errors.length != 0) {
             title = 'Your code has lint.';
-            rowClass = 'failure'
-            var table = buildReportTable(errors);
-            body.append(table);
-            body.show();
+            status_ = 'failure';
+            body = buildReportTable(errors);
         }
-        $('.report .diatribe_title').text(title);
-        $('.report_row').removeClass('success failure').addClass(rowClass).slideDown();
+
+        update(title, status_, body);
+    };
+
+    var displayError = function (error) {
+        var body = $('<div class="error_report"></div>');
+        body.append(error.toString());
+        update('Your code has an error', 'failure', body);
     };
 
     var runLinter = function () {
         var source = $('.editor').val();
-        var errors = coffeelint.lint(source);
-        displayReport(errors);
+        var errors = [];
+        var compileError = null;
+        try {
+            errors = coffeelint.lint(source);
+        } catch (e) {
+            compileError = e;
+        }
+        if (compileError) {
+            displayError(compileError);
+        } else {
+            displayReport(errors);
+        }
     };
 
     $(document).ready(function () {
